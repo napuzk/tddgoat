595f0cb2a92cb6401a18093a083e7adc8a096fcd
kzupan <kzupan@freetime.scc911.com>
2016-11-13 19:11:51 -0700

    CH8-4: Add gunicorn to requirements.txt for virtualenv

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -80,6 +80,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
